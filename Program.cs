f1be1230377a107375e5289910ea41768d877111
OSWALDO DURAN CHAVERRA <oswaldo.duran7575@unaula.edu.co>
2021-09-02 14:36:15 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Primer_parcial
+{
+    class Program
+    {
+        static void Main(string[] cadena)
+        {
+            Console.WriteLine("Seleccione una conversion");
+            Console.WriteLine("1.Cifrado");
+            Console.WriteLine("2.Descifrado");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n == 1)
+            {
+                string binaria = "";
+                int i = 0;
+                while (i < cadena.Length)
+                {
+                    if (i < cadena.Length)
+                    {
+                        binaria = binaria + cadena[i + 1].ToString() +
+                            cadena[i].ToString();
+                    }
+                    else
+                    {
+                        binaria = binaria + cadena[cadena.Length - 1].ToString();
+                    }
+                    i = i + 2;
+                }
+            }
+            else
+            {
+                string binaria = "";
+                int i = 0;
+                while (i < cadena.Length)
+                {
+                    if (i < cadena.Length)
+                    {
+                        binaria = binaria + cadena[i + 1].ToString() +
+                            cadena[i].ToString();
+                    }
+                    else
+                    {
+                        binaria = binaria + cadena[cadena.Length - 1].ToString();
+                    }
+                    i = i + 2;
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
